a6df90db1ad8c36bf2957c1bd55be19ccaef02a2
Tariq Khan <tariqcs129@gmail.com>
2023-07-25 00:35:04 -0700

    signUp, Login ,Firestore

---
+++ update.js
@@ -0,0 +1,35 @@
+import { auth,app,db } from "./firbase.mjs"
+import { doc, setDoc ,updateDoc  } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-firestore.js";
+
+
+const name= document.getElementById('name').value;
+const email= document.getElementById('email').value;
+const password= document.getElementById('psw').value;
+const address= document.getElementById('address').value;
+
+
+// const btn =document.getElementById('btn1')
+// btn.addEventListener('click',()=>{
+//     window.location.href='./update.html'
+
+// })
+const btn=document.getElementById("update")
+
+btn.addEventListener('click',async()=>{
+
+    const currentuid=(auth.currentUser.uid);
+    const washingtonRef = doc(db, "users", currentuid);
+    
+    
+    // Set the "capital" field of the city 'DC'
+    await updateDoc(washingtonRef, {
+        
+    name:name.value,
+        email:email.value,
+        password:password.value,
+        address:address.value,
+        
+
+});
+alert("Your Account has been updated")
+})
